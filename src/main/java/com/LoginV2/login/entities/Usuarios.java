1d8febb2c218e36a65213bd5099d60135d1965db
Ivan Chavez <chavezivan4444@gmail.com>
2025-04-08 16:43:25 -0700

    Update Usuarios.java
    Actualizar a nuevas columna del Usuario

--- src/main/java/com/LoginV2/login/entities/Usuarios.java
+++ src/main/java/com/LoginV2/login/entities/Usuarios.java
@@ -1,6 +1,8 @@
 package com.LoginV2.login.entities;
 
 
+import java.sql.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,9 +24,72 @@ public class Usuarios {
     private Long id;
 
     @NotBlank
+    @Column(name = "correo")
     private String Correo;
 
     @NotBlank
     @Column(name = "contraseña")  
-    private String contrasena; 
+    private String contrasena;
+    
+    @NotBlank
+    @Column(name = "rol_sistema")
+    private String rolSistema;
+
+    @NotBlank
+    @Column(name = "estatus")
+    private String estatus;
+
+    @NotBlank
+    @Column(name = "nombre")
+    private String nombre;
+
+    @NotBlank
+    @Column(name = "apellidos")
+    private String apellidos;
+
+    @NotBlank
+    @Column(name = "fecha_nacimiento")
+    private Date fechaNacimiento;
+
+    @NotBlank
+    @Column(name = "edad")
+    private int edad;
+
+    @NotBlank
+    @Column(name = "rfc")
+    private String rfc;
+
+    @NotBlank
+    @Column(name = "curp")
+    private String curp;
+    
+    @NotBlank
+    @Column(name = "telefono")
+    private String telefono;
+
+    
+    @Column(name = "foto")
+    private byte foto;
+
+    @NotBlank
+    @Column(name = "fecha_ingreso")
+    private Date fechaIngreso;
+
+    @NotBlank
+    @Column(name = "puesto")
+    private String puesto;
+
+    @NotBlank
+    @Column(name = "costo_hora")
+    private double costoHora;
+
+    @NotBlank
+    @Column(name = "sueldo_liquido")
+    private double sueldoLiquido;
+
+    @NotBlank
+    @Column(name = "sueldo_neto")
+    private double sueldoNeto;
+
+
 }
